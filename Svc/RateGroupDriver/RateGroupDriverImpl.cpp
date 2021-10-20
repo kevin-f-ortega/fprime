e0d09ec910d31136a7a0e409842417ec92744d66
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2021-10-19 22:26:55 -0700

    adding time tracking to rategroup driver

--- Svc/RateGroupDriver/RateGroupDriverImpl.cpp
+++ Svc/RateGroupDriver/RateGroupDriverImpl.cpp
@@ -4,6 +4,8 @@
 #include <Fw/Types/Assert.hpp>
 #include <stdio.h>
 
+#include <Fw/Logger/Logger.hpp>
+
 namespace Svc {
 
     RateGroupDriverImpl::RateGroupDriverImpl(const char* compName, I32 dividers[], I32 numDividers) :
@@ -51,6 +53,9 @@ namespace Svc {
   uint32_t lowest_usec = threshold;
   uint32_t average_usec = 0;
   uint32_t average_isr_usec = 0;
+  uint32_t isr_out_of_bounds = 0;
+  uint32_t isr_out_of_bounds_0 = 0;
+  uint32_t slipped = 0;
   uint32_t skipped = 0;
 
   for(uint32_t i = 0; i < len; i++)
@@ -58,34 +63,45 @@ namespace Svc {
     if(elapsed_time[i].cycle == 0)
     {
       skipped++;
-      printf("skipping entry %u\n", i);
+      Fw::Logger::logMsg("skipping entry %u\n", i);
       continue;
     }
 
     if(i < len)
     {
       uint32_t delta_isr_usec = 100;
-      uint32_t diff_isr = elapsed_time[i+1].isr_start.diffUSec(elapsed_time[i].isr_start);
+	    Os::IntervalTimer::RawTime isr1 = elapsed_time[i].isr_start.getTimerVal();
+	    Os::IntervalTimer::RawTime isr2 = elapsed_time[i+1].isr_start.getTimerVal();
+      uint32_t diff_isr = Os::IntervalTimer::getDiffUsec(isr2, isr1);
       if(diff_isr > (threshold + delta_isr_usec) ||
          diff_isr < (threshold - delta_isr_usec))
       {
-	      Os::IntervalTimer::RawTime isr1 = elapsed_time[i].start.getTimerVal();
-	      Os::IntervalTimer::RawTime isr2 = elapsed_time[i+1].start.getTimerVal();
+#if 0
+	      //Os::IntervalTimer::RawTime isr1 = elapsed_time[i].start.getTimerVal();
+	      //Os::IntervalTimer::RawTime isr2 = elapsed_time[i+1].start.getTimerVal();
         printf("ISR1 counter %u time is %u.%u\n", i, isr1.upper, isr1.lower);
         printf("ISR2 counter %u time is %u.%u\n", i+1, isr2.upper, isr2.lower);
         printf("ISR Diff time is %u usecs\n", diff_isr);
+#endif
+        isr_out_of_bounds++;
+        if(diff_isr == 0) { isr_out_of_bounds_0++; }
       }
       average_isr_usec += diff_isr;
     }
 
-    uint32_t diff_usecs = elapsed_time[i].end.diffUSec(elapsed_time[i].start);
+    Os::IntervalTimer::RawTime endtime = elapsed_time[i].end.getTimerVal();
+    Os::IntervalTimer::RawTime starttime = elapsed_time[i].start.getTimerVal();
+    uint32_t diff_usecs = Os::IntervalTimer::getDiffUsec(endtime, starttime);
     if(diff_usecs > threshold)
     {
-      Os::IntervalTimer::RawTime endtime = elapsed_time[i].end.getTimerVal();
-      Os::IntervalTimer::RawTime starttime = elapsed_time[i].start.getTimerVal();
+#if 0
+      //Os::IntervalTimer::RawTime endtime = elapsed_time[i].end.getTimerVal();
+      //Os::IntervalTimer::RawTime starttime = elapsed_time[i].start.getTimerVal();
       printf("Cycle Slip %u: \n", elapsed_time[i].cycle);
       printf("\t\tend time  : %u.%u\n", endtime.upper, endtime.lower);
       printf("\t\tstart time: %u.%u\n", starttime.upper, starttime.lower);
+#endif
+      slipped++;
     }
     highest_usec = (diff_usecs > highest_usec) ? diff_usecs : highest_usec;
     lowest_usec = (diff_usecs < lowest_usec) ? diff_usecs : lowest_usec;
@@ -93,6 +109,9 @@ namespace Svc {
   }
   FW_ASSERT(len > skipped);
   average_usec /= (len-skipped);
+  printf("number of ISR out of bounds: %u\n", isr_out_of_bounds);
+  printf("number of ISR with 0 diff: %u\n", isr_out_of_bounds_0);
+  printf("number of cycle slips: %u\n", slipped);
   printf("highest duration: %u usec\n", highest_usec);
   printf("lowest duration : %u usec\n", lowest_usec);
   printf("average duration: %u usec\n", average_usec);
@@ -103,7 +122,15 @@ namespace Svc {
     void RateGroupDriverImpl::CycleIn_handler(NATIVE_INT_TYPE portNum, Svc::TimerVal& cycleStart) {
 
 	      U32 prev_interrupt_counter = 0;
-	      if(portNum == RATE_GROUP_NUM)
+
+        // Loop through each divider. For a given port, the port will be called when the divider value
+        // divides evenly into the number of ticks. For example, if the divider value for a port is 4,
+        // it would be called every fourth invocation of the CycleIn port.
+        for (NATIVE_INT_TYPE entry = 0; entry < static_cast<NATIVE_INT_TYPE>(FW_NUM_ARRAY_ELEMENTS(this->m_dividers)); entry++) {
+            if (this->m_dividers[entry] != 0) {
+                if (this->isConnected_CycleOut_OutputPort(entry)) {
+                    if ((this->m_ticks % this->m_dividers[entry]) == 0) {
+	      if(entry == RATE_GROUP_NUM)
 	      {
 		      prev_interrupt_counter = interrupt_counter;
 		      if(prev_interrupt_counter < MAX_LOOP)
@@ -114,24 +141,8 @@ namespace Svc {
 		      }
 	      }
 
-        // Loop through each divider. For a given port, the port will be called when the divider value
-        // divides evenly into the number of ticks. For example, if the divider value for a port is 4,
-        // it would be called every fourth invocation of the CycleIn port.
-        for (NATIVE_INT_TYPE entry = 0; entry < static_cast<NATIVE_INT_TYPE>(FW_NUM_ARRAY_ELEMENTS(this->m_dividers)); entry++) {
-            if (this->m_dividers[entry] != 0) {
-                if (this->isConnected_CycleOut_OutputPort(entry)) {
-                    if ((this->m_ticks % this->m_dividers[entry]) == 0) {
                         this->CycleOut_out(entry,cycleStart);
-                    }
-                }
-            }
-        }
-        
-        // rollover the tick value when the tick count reaches the rollover value
-        // the rollover value is the product of all the dividers. See comment in constructor.
-        this->m_ticks = (this->m_ticks + 1) % this->m_rollover;
-
-        if(portNum == RATE_GROUP_NUM)
+        if(entry == RATE_GROUP_NUM)
         {
           if(prev_interrupt_counter < MAX_LOOP)
           {
@@ -143,10 +154,26 @@ namespace Svc {
           static bool done = false;
           if(!done && prev_interrupt_counter >= MAX_LOOP)
           {
+            //Os::IntervalTimer::RawTime t = cycleStart.getTimerVal();
+            //Fw::Logger::logMsg("cycleStart -> %u.%u\n", t.upper, t.lower);
+            //Os::IntervalTimer::RawTime t100 = elapsed_times[100].isr_start.getTimerVal();
+            //Os::IntervalTimer::RawTime t101 = elapsed_times[101].isr_start.getTimerVal();
+            //Fw::Logger::logMsg("random isr start 100 -> %u.%u\n", t100.upper, t100.lower);
+            //Fw::Logger::logMsg("random isr start 101 -> %u.%u\n", t101.upper, t101.lower);
+            //Fw::Logger::logMsg("diff 101 - 100 isr start -> %u\n", Os::IntervalTimer::getDiffUsec(t101, t100));
             print_elapsed_times(elapsed_times, MAX_LOOP-1, CYCLE_SLIP_THRESHOLD); // last entry is 0
             done = true;
           }
         }
+                    }
+                }
+            }
+        }
+        
+        // rollover the tick value when the tick count reaches the rollover value
+        // the rollover value is the product of all the dividers. See comment in constructor.
+        this->m_ticks = (this->m_ticks + 1) % this->m_rollover;
+
     }
 
 }
